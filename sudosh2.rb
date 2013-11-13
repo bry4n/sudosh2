bfe48f787d69c8e0c963144b582ed516de93980e
bry4n <bryann83@gmail.com>
2013-11-12 21:07:43 -0600

    using git instead

--- sudosh2.rb
+++ sudosh2.rb
@@ -3,9 +3,9 @@ require 'formula'
 # brew install sudosh2.rb
 class Sudosh2 < Formula
   homepage "http://sourceforge.net/projects/sudosh2/"
-  url "https://github.com/bry4n/sudosh2/archive/v2.tar.gz"
-  #md5 "f4218e28e4bc87cde88f176d0a39c09c"
-  sha1 "8dc6efacf0b5a825a5ed13d1c8a90f0c0bae6562"
+  head "https://github.com/bry4n/sudosh2.git"
+  url "https://github.com/bry4n/sudosh2.git", :revision => "816caafc25a342bc822152496dd2489dab2a89ff"
+  #sha1 "8dc6efacf0b5a825a5ed13d1c8a90f0c0bae6562"
 
   def install
     system "./configure", "--disable-debug",

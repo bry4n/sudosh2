d46612c6b68b0c8598ba6fcb1500b7d1921c81d5
bry4n <bryann83@gmail.com>
2013-11-12 20:42:18 -0600

    Add homebrew script

---
+++ sudosh2.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+# brew install sudosh2.rb
+class Sudosh2 < Formula
+  homepage "http://sourceforge.net/projects/sudosh2/"
+  url "https://github.com/bry4n/sudosh2/archive/v2.tar.gz"
+  #md5 "f4218e28e4bc87cde88f176d0a39c09c"
+  sha1 "8dc6efacf0b5a825a5ed13d1c8a90f0c0bae6562"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+
+end
